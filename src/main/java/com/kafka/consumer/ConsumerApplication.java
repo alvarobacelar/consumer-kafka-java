dc0c3d33f4937165d5574e37457d89ec291deeee
Álvaro Bacelar <alvarobacelar@outlook.com>
2019-04-14 19:46:28 -0300

    Alteração da saída debug

--- src/main/java/com/kafka/consumer/ConsumerApplication.java
+++ src/main/java/com/kafka/consumer/ConsumerApplication.java
@@ -55,22 +55,21 @@ public class ConsumerApplication {
 		int noMessageFound = 0;
 		while (true) {
 			ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
-			// 1000 is the time in milliseconds consumer will wait if no record is found at
-			// broker.
 			if (consumerRecords.count() == 0) {
 				noMessageFound++;
 				if (noMessageFound > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
-					// If no message found count is reached to threshold exit loop.
 					break;
 				else
 					continue;
 			}
 			// print each record.
 			consumerRecords.forEach(record -> {
+				System.out.printf("------------ message consumer --------- \n");
 				System.out.println("Record Key " + record.key());
 				System.out.println("Record value " + record.value());
 				System.out.println("Record partition " + record.partition());
 				System.out.println("Record offset " + record.offset());
+				System.out.println("Server Kafka " + kafka_server);
 			});
 			// commits the offset of record to broker.
 			consumer.commitAsync();
